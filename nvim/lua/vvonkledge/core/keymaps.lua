ccfe514e8d7430bdddadbcdc1608cf7c929c2e20
Alexandre LEROY <alexandre.leroy@turtleshell.fr>
2024-07-13 19:50:04 +0200

    add lazygit and nvim-tree configuration

--- nvim/lua/vvonkledge/core/keymaps.lua
+++ nvim/lua/vvonkledge/core/keymaps.lua
@@ -13,3 +13,5 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>p", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle nvim tree" }) -- close current split window
+keymap.set("n", "<leader>ff", "<cmd>NvimTreeFindFile<CR>", { desc = "Toggle nvim tree" }) -- close current split window
